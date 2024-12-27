a8026bb7a9c24f3fa493d22a4b89b9b08c87fef0
Груданов Николай <grudanov.n.a@gmail.com>
2024-12-27 21:06:32 +0300

    в config добавил модель

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ gigachat_scope = os.getenv("GIGACHAT_SCOPE", "GIGACHAT_API_PERS")
 gigachat_auth_key = os.getenv("GIGACHAT_AUTH_KEY")
 gigachat_model = "GigaChat-Max"
 
-predict_model = "bert"  # bert | rfr
+predict_model = "bert"  # bert | rfr | lstm
